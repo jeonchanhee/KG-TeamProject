0f3858b4a16b7d1af5718791cfcc52bdb0386d21
young0915 <38437784+young0915@users.noreply.github.com>
2020-02-04 15:35:38 +0900

    플레이어헤더
    헤더 한번 보세요

--- player.h
+++ player.h
@@ -1,14 +1,29 @@
 #pragma once
 #include "gameNode.h"
+
+enum PLAYERVERSION
+{
+	SHOP_PLAYER_VERSION,
+	//플레이어
+};
+
+struct tagplaeyr
+{
+
+};
 class player : public gameNode
 {
-private : 
+private:
 	RECT _plauer;
 
-
-public :
+public:
 	player();
 	~player();
+
+	HRESULT init();
+	void release();
+	void update();
+	void render();
 };
 
 /*
