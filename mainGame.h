300cff9c858a2e18f50b70b5e426e538867496e7
찬희 전 <wjscskgml@naver.com>
2020-02-04 14:21:30 +0900

    기본베이스

---
+++ mainGame.h
@@ -0,0 +1,22 @@
+#pragma once
+#include"gameNode.h"
+
+class mainGame : public gameNode
+{
+private:
+
+	
+
+public:
+	mainGame();
+	~mainGame();
+
+	virtual HRESULT init();
+	virtual void release();
+	virtual void update();
+	virtual void render(/*HDC hdc*/);
+
+
+
+};
+
